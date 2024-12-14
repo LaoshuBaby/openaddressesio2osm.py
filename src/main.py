a98792df6f63c5ca120f52f03f068f228c208918
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-12-15 06:24:19 +0900

    feat

--- src/main.py
+++ src/main.py
@@ -1,28 +1,31 @@
 import os
 import json
 
-TARGET_PATH=input("Pls paste file path: ")
-TARGET_PATH=TARGET_PATH[1:-1] if (TARGET_PATH[0] == "\"" and TARGET_PATH[-1] == '"') else TARGET_PATH
+TARGET_PATH = input("Pls paste file path: ")
+TARGET_PATH = (
+    TARGET_PATH[1:-1]
+    if (TARGET_PATH[0] == '"' and TARGET_PATH[-1] == '"')
+    else TARGET_PATH
+)
 
 
 def main():
     # print(TARGET_PATH)
-    with open(TARGET_PATH,"r",encoding="utf-8") as f:
-        raw_data=list(filter(bool,f.read().split("\n")))
-    data=[]
-    THERSHOLD=1000000
-    counter=0
+    with open(TARGET_PATH, "r", encoding="utf-8") as f:
+        raw_data = list(filter(bool, f.read().split("\n")))
+    data = []
+    THERSHOLD = 1000000
+    counter = 0
     for single_raw_data in raw_data:
-        if counter<=THERSHOLD:
-            if counter==42:
+        if counter <= THERSHOLD:
+            if counter == 42:
                 print(single_raw_data)
             data.append(json.loads(single_raw_data))
-            counter+=1
+            counter += 1
         else:
             break
-    
-    print(data[42])
 
+    print(data[42])
 
 
 if __name__ == "__main__":
---
+++ src/sample.py
@@ -0,0 +1,14 @@
+import os
+
+with open("openaddressesio.jp.tokyo.geojson", "r", encoding="utf-8") as f:
+    content=f.read().split("\n")
+
+data=[]
+
+
+for i in range(len(content)):
+    if i%100==0:
+        data.append(content[i])
+    
+with open("lite.geojson","w",encoding="utf-8") as f:
+    f.write("\n".join(data))
