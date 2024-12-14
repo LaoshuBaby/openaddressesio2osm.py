825dbf8f8754e4330b89b4e17dbe1c9427a7d21e
快乐的老鼠宝宝 <laoshubaby@protonmail.com>
2024-12-15 06:40:10 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,21 +11,43 @@ TARGET_PATH = (
 
 def main():
     # print(TARGET_PATH)
+
     with open(TARGET_PATH, "r", encoding="utf-8") as f:
         raw_data = list(filter(bool, f.read().split("\n")))
-    data = []
+    raw_json = []
     THERSHOLD = 1000000
     counter = 0
     for single_raw_data in raw_data:
         if counter <= THERSHOLD:
-            if counter == 42:
-                print(single_raw_data)
-            data.append(json.loads(single_raw_data))
+            raw_json.append(json.loads(single_raw_data))
             counter += 1
         else:
             break
+    
+    data=[]
+    for single_raw_json in raw_json:
+        temp_dict={**{
+            "coordinates":single_raw_json["geometry"]["coordinates"]
+        },**single_raw_json["properties"]}
+
+        data.append({
+            "id":temp_dict["coordinates"],
+            "postcode":temp_dict["postcode"],
+            "region":temp_dict["region"],
+            "district":temp_dict["district"],
+            "city":temp_dict["city"],"street":temp_dict["street"],
+            "number":temp_dict["number"],
+            
+            "unit":temp_dict["unit"],
+            "coordinates":{
+                "lon":temp_dict["coordinates"][0],"lat":temp_dict["coordinates"][1]
+            }
+        })
 
     print(data[42])
+
+    
+
 
 
 if __name__ == "__main__":
